0cad92c577b2c838b08c1e42a95cf43304a4b42e
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-01-05 15:40:11 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// let counter = 0;
+// let dom = document.querySelector("#root");
+// doCount();
+
+// let h1 = document.querySelector("#h1");
+// doText();
+
+// function doCount() {
+//     counter++;
+//     let element = React.createElement("p", {}, "count: " + counter);
+//     ReactDOM.render(element, dom);
+// }
+
+// function doText() {
+//     let addText = React.createElement("h1", {}, "reactを書き換えました");
+//     ReactDOM.render(addText, h1);
+// }
+
+// let dom = document.querySelector("#root");
+
+// let element = React.createElement("div", {}, [
+//     React.createElement("h2", {}, "Hello!"),
+//     React.createElement("h4", { className: "alert alert-primary" }, "React sample page."),
+//     React.createElement("ul", { className: "list-group" }, [
+//         React.createElement("li", { className: "list-group-item" }, "First item."),
+//         React.createElement("li", { className: "list-group-item" }, "Second item."),
+//         React.createElement("li", { className: "list-group-item" }, "Third item."),
+//     ]),
+// ]);
+
+// ReactDOM.render(element, dom);
